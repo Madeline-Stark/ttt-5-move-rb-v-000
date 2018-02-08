b5c8ec237416cafb11212fca57e94b1dac475c8f
Madeline Stark <madeline.stark1@gmail.com>
2018-02-08 21:39:50 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index(user_input)
 end
 
 
-def move(display_board, user_input, X_or_O)
-  puts user_input(display_board)
+def move(display_board, user_input, X_or_O = X)
+  display_board(user_input)
 end
